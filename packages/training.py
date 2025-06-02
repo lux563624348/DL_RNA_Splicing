3c7b5a8addf1e76dc9f660c610b3a01672801f53
lux563624348 <lux@gwu.edu>
2025-06-02 18:23:53 -0400

    Add Pangolin Seq

--- packages/training.py
+++ packages/training.py
@@ -44,6 +44,48 @@ class ResBlock(nn.Module):
         out = out + x
         return out
 
+class Pangolin(nn.Module):
+    def __init__(self, L, W, AR):
+        super(Pangolin, self).__init__()
+        self.n_chans = L
+        self.conv1 = nn.Conv1d(4, L, 1)
+        self.skip = nn.Conv1d(L, L, 1)
+        self.resblocks, self.convs = nn.ModuleList(), nn.ModuleList()
+        for i in range(len(W)):
+            self.resblocks.append(ResBlock(L, W[i], AR[i]))
+            if (((i + 1) % 4 == 0) or ((i + 1) == len(W))):
+                self.convs.append(nn.Conv1d(L, L, 1))
+        self.conv_last1 = nn.Conv1d(L, 2, 1)
+        self.conv_last2 = nn.Conv1d(L, 1, 1)
+        self.conv_last3 = nn.Conv1d(L, 2, 1)
+        self.conv_last4 = nn.Conv1d(L, 1, 1)
+        self.conv_last5 = nn.Conv1d(L, 2, 1)
+        self.conv_last6 = nn.Conv1d(L, 1, 1)
+        self.conv_last7 = nn.Conv1d(L, 2, 1)
+        self.conv_last8 = nn.Conv1d(L, 1, 1)
+
+    def forward(self, x):
+        conv = self.conv1(x)
+        skip = self.skip(conv)
+        j = 0
+        for i in range(len(W)):
+            conv = self.resblocks[i](conv)
+            if (((i + 1) % 4 == 0) or ((i + 1) == len(W))):
+                dense = self.convs[j](conv)
+                j += 1
+                skip = skip + dense
+        CL = 2 * np.sum(AR * (W - 1))
+        skip = F.pad(skip, (-CL // 2, -CL // 2))
+        out1 = F.softmax(self.conv_last1(skip), dim=1)
+        out2 = torch.sigmoid(self.conv_last2(skip))
+        out3 = F.softmax(self.conv_last3(skip), dim=1)
+        out4 = torch.sigmoid(self.conv_last4(skip))
+        out5 = F.softmax(self.conv_last5(skip), dim=1)
+        out6 = torch.sigmoid(self.conv_last6(skip))
+        out7 = F.softmax(self.conv_last7(skip), dim=1)
+        out8 = torch.sigmoid(self.conv_last8(skip))
+        return torch.cat([out1, out2, out3, out4, out5, out6, out7, out8], 1)
+        
 class PangolinEXP(nn.Module):
     def __init__(self, L, W, AR):
         super(PangolinEXP, self).__init__()
