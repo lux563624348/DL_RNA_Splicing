a96eb595a046f809884ec9f352583def270ca5f0
lux563624348 <lux@gwu.edu>
2025-06-04 02:24:28 -0400

    Bug fix for Run_Training.sh

--- packages/.ipynb_checkpoints/Run_Training-checkpoint.sh
+++ packages/.ipynb_checkpoints/Run_Training-checkpoint.sh
@@ -4,4 +4,4 @@ aws s3 cp $AWS_S3_PATH/${Data_ID} ./${Data_ID}
 
 python ./packages/training.py --input ./${Data_ID} --epochs 200 --model seq --output ${Data_ID/.pt/_model.pt}
 
-aws s3 cp ${Data_ID/.pt/_model.pt} $AWS_S3_PATH
+aws s3 cp ${Data_ID/.pt/_model.pt} $AWS_S3_PATH/
